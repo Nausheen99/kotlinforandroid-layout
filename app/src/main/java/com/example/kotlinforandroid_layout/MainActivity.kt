d2e8077be4c6f3a149d9fc46abdb07ec7abe1e88
nausheen99 <naushabbas@gmail.com>
2022-04-05 15:36:00 +0500

    2.0

--- app/src/main/java/com/example/kotlinforandroid_layout/MainActivity.kt
+++ app/src/main/java/com/example/kotlinforandroid_layout/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.kotlinforandroid_layout
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawable
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bitman = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources,bitman)
+
+        rounded.cornerRadius = 20f
+        logo.setImageDrawable(rounded)
     }
 }
